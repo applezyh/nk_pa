c3c6056f6bfb9d966a38b9db0ef92fc2fffdc1bc
tracer-ics2017 <tracer@njuics.org>
2022-02-17 16:35:11 +0800

    >  compile 161220000 apple Linux apple-VirtualBox 5.13.0-28-generic #31~20.04.1-Ubuntu SMP Wed Jan 19 14:08:10 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:35:11 up 14:54,  1 user,  load average: 0.57, 0.26, 0.13 afb694311b38fc46a3840efe73ea886591d9c7f0

--- nemu/src/cpu/exec/control.c
+++ nemu/src/cpu/exec/control.c
@@ -24,15 +24,15 @@ make_EHelper(jmp_rm) {
 
 make_EHelper(call) {
   decoding.jmp_eip = id_dest->val+*eip;
-  rtl_push(eip);
+  rtl_push(*eip);
   decoding.is_jmp = 1;
   // TODO();
   print_asm("call %x", decoding.jmp_eip);
 }
 
 make_EHelper(ret) {
-  TODO();
-
+  // TODO();
+  rtl_pop(*eip);
   print_asm("ret");
 }
 
