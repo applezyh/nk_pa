0c044c4035f3ec9deccd24d3c51e9240b559355c
tracer-ics2017 <tracer@njuics.org>
2022-02-17 17:16:28 +0800

    >  compile 161220000 apple Linux apple-VirtualBox 5.13.0-28-generic #31~20.04.1-Ubuntu SMP Wed Jan 19 14:08:10 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:16:28 up 15:35,  1 user,  load average: 0.00, 0.02, 0.00 fa9a9d764621d45bc8ba9654299f514b6d64c665

--- nemu/include/cpu/reg.h
+++ nemu/include/cpu/reg.h
@@ -96,7 +96,6 @@ extern const char* regsw[];
 extern const char* regsb[];
 
 static inline const char* reg_name(int index, int width) {
-  printf("%d\n",width);
   assert(index >= 0 && index < 8);
   switch (width) {
     case 4: return regsl[index];
