55f7fc5efd7f3ee305c745410e74ea135eb2e651
tracer-ics2017 <tracer@njuics.org>
2022-02-17 14:16:50 +0800

    >  compile 161220000 apple Linux apple-VirtualBox 5.13.0-28-generic #31~20.04.1-Ubuntu SMP Wed Jan 19 14:08:10 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:16:50 up 12:35,  1 user,  load average: 0.03, 0.07, 0.08 53ea3a098a5672d9476d168627506da7554fc07f

--- nemu/include/cpu/rtl.h
+++ nemu/include/cpu/rtl.h
@@ -143,7 +143,7 @@ static inline void rtl_push(const rtlreg_t* src1) {
   // esp <- esp - 4
   // M[esp] <- src1
   cpu.esp-=4;
-  vaddr_write(cpu.esp, src1, 4);
+  vaddr_write(cpu.esp, 4, src1);
   //TODO();
 }
 
