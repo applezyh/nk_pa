4bddf4c9642f676a127e14bc7821d51400f14837
tracer-ics2017 <tracer@njuics.org>
2022-02-17 13:07:43 +0800

    >  compile 161220000 apple Linux apple-VirtualBox 5.13.0-28-generic #31~20.04.1-Ubuntu SMP Wed Jan 19 14:08:10 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  13:07:43 up 11:26,  1 user,  load average: 0.02, 0.02, 0.01 9484f2c14016266bbe682e84e0681022d8dae95e

--- nemu/src/cpu/decode/decode.c
+++ nemu/src/cpu/decode/decode.c
@@ -28,9 +28,8 @@ make_DHelper(Av) {
   Operand* op=id_dest;
   op->type = OP_TYPE_IMM;
   op->imm = instr_fetch(eip, op->width);
-  printf("%d\n", op->imm);
   rtl_li(&op->val, op->imm+eip);
-  printf("%x %x %x\n",eip,op->val);
+  printf("%x %x\n",eip,op->val);
 
 #ifdef DEBUG
   snprintf(op->str, OP_STR_SIZE, "$0x%x", op->imm);
