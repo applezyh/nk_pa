b512c508d6f094e12571d4bb9d8f4118ae4e6147
tracer-ics2017 <tracer@njuics.org>
2022-02-20 02:06:25 +0800

    >  compile 161220000 apple Linux apple-VirtualBox 5.13.0-28-generic #31~20.04.1-Ubuntu SMP Wed Jan 19 14:08:10 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  02:06:25 up 1 day,  9:47,  1 user,  load average: 0.03, 0.06, 0.11 b2de364d350d13e7e60f04add6fbbe649f34e05d

--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -51,7 +51,7 @@ make_group(gp2,
 
   /* 0xf6, 0xf7 */
 make_group(gp3,
-    EXW(test,1), EMPTY, EXW(not, 4), EMPTY,
+    IDEXW(I2r,test,1), EMPTY, EXW(not, 4), EMPTY,
     EXW(mul, 4), EXW(imul2, 4), EMPTY, EXW(idiv,4))
 
   /* 0xfe */
