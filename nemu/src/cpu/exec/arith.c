f6ab1f47a521c5f33581dc052dd9991831d781e3
tracer-ics2017 <tracer@njuics.org>
2022-02-19 20:15:35 +0800

    >  compile 161220000 apple Linux apple-VirtualBox 5.13.0-28-generic #31~20.04.1-Ubuntu SMP Wed Jan 19 14:08:10 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:15:35 up 1 day,  4:35,  1 user,  load average: 0.01, 0.05, 0.01 ba273cb78e4c862655ca3c13fc07ee1f6526332

--- nemu/src/cpu/exec/arith.c
+++ nemu/src/cpu/exec/arith.c
@@ -96,8 +96,21 @@ make_EHelper(inc) {
 make_EHelper(dec) {
   // TODO();
   uint32_t sub_num=1;
-  rtl_sub(&id_dest->val, &id_dest->val, &sub_num);
-  operand_write(id_dest,&id_dest->val);
+  rtl_sub(&t2, &id_dest->val, &sub_num);
+  rtl_sltu(&t3, &id_dest->val, &t2);
+
+  rtl_update_ZFSF(&t2, id_dest->width);
+  
+  rtl_sltu(&t0, &id_dest->val, &t2);
+
+  rtl_or(&t0, &t3, &t0);
+  rtl_set_CF(&t0);
+
+  rtl_xor(&t0, &id_dest->val, &id_src->val);
+  rtl_xor(&t1, &id_dest->val, &t2);
+  rtl_and(&t0, &t0, &t1);
+  rtl_msb(&t0, &t0, id_dest->width);
+  rtl_set_OF(&t0);
 
   print_asm_template1(dec);
 }
