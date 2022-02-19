f1b660934b1eca1e64f7ded4e23dc82fcc6ef017
tracer-ics2017 <tracer@njuics.org>
2022-02-19 23:58:27 +0800

    >  compile 161220000 apple Linux apple-VirtualBox 5.13.0-28-generic #31~20.04.1-Ubuntu SMP Wed Jan 19 14:08:10 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:58:27 up 1 day,  7:39,  1 user,  load average: 0.21, 0.14, 0.11 94e7648b867b68bd9817d55e59452fb448b789a

--- nemu/src/cpu/exec/arith.c
+++ nemu/src/cpu/exec/arith.c
@@ -47,7 +47,7 @@ make_EHelper(cmp) {
   rtl_sub(&t2, &id_dest->val, &id_src->val);
   rtl_sltu(&t3, &id_dest->val, &t2);
 
-  printf("%d - %d = %x",id_dest->val,id_src->val,t2);
+  if(cpu.eip==0x1000c0) printf("%d - %d = %x\n",id_dest->val,id_src->val,t2);
 
   rtl_update_ZFSF(&t2, id_dest->width);
   
