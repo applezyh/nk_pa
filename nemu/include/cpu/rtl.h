74312cf194693861b24f86d7ebd386cebc3c4ebc
tracer-ics2017 <tracer@njuics.org>
2022-02-19 14:22:21 +0800

    >  compile 161220000 apple Linux apple-VirtualBox 5.13.0-28-generic #31~20.04.1-Ubuntu SMP Wed Jan 19 14:08:10 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:22:21 up 22:45,  1 user,  load average: 0.32, 0.21, 0.12 d57d60769dc75b878c4bcb3985e0460ddfb5410

--- nemu/include/cpu/rtl.h
+++ nemu/include/cpu/rtl.h
@@ -193,9 +193,10 @@ static inline void rtl_update_ZF(const rtlreg_t* result, int width) {
   // TODO();
   uint32_t zf_flag = (*result==0);
   rtl_set_ZF(&zf_flag);
+  if(zf_flag){
   uint32_t a;
   rtl_get_ZF(&a);
-  printf("%d %d\n",zf_flag,a);
+  printf("%d %d\n",zf_flag,a);}
 }
 
 static inline void rtl_update_SF(const rtlreg_t* result, int width) {
