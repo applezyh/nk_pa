be817cef6cb58c57ab2fa32ef8a91b4894516c46
tracer-ics2017 <tracer@njuics.org>
2022-02-20 06:08:55 +0800

    >  compile 161220000 apple Linux apple-VirtualBox 5.13.0-28-generic #31~20.04.1-Ubuntu SMP Wed Jan 19 14:08:10 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  06:08:55 up 1 day, 13:49,  1 user,  load average: 0.03, 0.03, 0.05 8a5a1220c82417655004c0045b1aef6fbc34726

--- nemu/include/cpu/rtl.h
+++ nemu/include/cpu/rtl.h
@@ -147,7 +147,8 @@ static inline void rtl_not(rtlreg_t* dest) {
 static inline void rtl_sext(rtlreg_t* dest, const rtlreg_t* src1, int width) {
   // dest <- signext(src1[(width * 8 - 1) .. 0])
   // TODO();
-  *dest=(uint32_t)(int32_t)(*src1);
+  *dest=(uint32_t)((int32_t)(*src1));
+  printf("%x %x\n",*dest,*src1);
 }
 
 static inline void rtl_push(const rtlreg_t* src1) {
