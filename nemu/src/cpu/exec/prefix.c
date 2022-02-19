56b9009eda78e3c5211e141e429d3e35ac1b072a
tracer-ics2017 <tracer@njuics.org>
2022-02-19 21:42:15 +0800

    >  compile 161220000 apple Linux apple-VirtualBox 5.13.0-28-generic #31~20.04.1-Ubuntu SMP Wed Jan 19 14:08:10 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:42:15 up 1 day,  5:36,  1 user,  load average: 0.13, 0.07, 0.03 a8c8125dd183f24f136683b8b33cab07a77e1dd0

--- nemu/src/cpu/exec/prefix.c
+++ nemu/src/cpu/exec/prefix.c
@@ -4,7 +4,6 @@ make_EHelper(real);
 
 make_EHelper(operand_size) {
   decoding.is_operand_size_16 = true;
-  memset(decoding.asm_buf,0,128);
   instr_fetch(eip, 1);
   exec_real(eip);
   decoding.is_operand_size_16 = false;
