9c9d3e8fe11eb377c9ff43b9605672f774fd1883
tracer-ics2017 <tracer@njuics.org>
2022-02-19 16:00:36 +0800

    >  compile 161220000 apple Linux apple-VirtualBox 5.13.0-28-generic #31~20.04.1-Ubuntu SMP Wed Jan 19 14:08:10 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:00:36 up 1 day, 23 min,  1 user,  load average: 0.01, 0.02, 0.00 989a51b949a3d24b7ddb5418c3b7657f70890b5a

--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -239,7 +239,6 @@ void exec_wrapper(bool print_flag) {
 
 #ifdef DEBUG
   int instr_len = decoding.seq_eip - cpu.eip;
-  if(instr_len>50||instr_len<0) instr_len=32;
 
   memset(decoding.asm_buf+(128-80),0,80);
   my_strcat(decoding.asm_buf, decoding.assembly);
