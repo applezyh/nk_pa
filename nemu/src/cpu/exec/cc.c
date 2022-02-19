257ab33bdc09e33bd082e044a3bbe44e869c54ec
tracer-ics2017 <tracer@njuics.org>
2022-02-19 14:46:48 +0800

    >  compile 161220000 apple Linux apple-VirtualBox 5.13.0-28-generic #31~20.04.1-Ubuntu SMP Wed Jan 19 14:08:10 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:46:48 up 23:10,  1 user,  load average: 0.00, 0.01, 0.03 e70116a87c098e685dd6f76d2c35f2a66b8a633

--- nemu/src/cpu/exec/cc.c
+++ nemu/src/cpu/exec/cc.c
@@ -19,7 +19,7 @@ void rtl_setcc(rtlreg_t* dest, uint8_t subcode) {
     case CC_B:
     case CC_E:
       rtl_get_ZF(dest);
-      printf("%d\n",*dest);
+      //printf("%d\n",*dest);
       break;
     case CC_BE:
     case CC_S:
