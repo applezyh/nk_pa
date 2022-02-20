b677d1c3a49d32ef35c88968c7e018c6197a1cf7
tracer-ics2017 <tracer@njuics.org>
2022-02-20 18:59:05 +0800

    >  run 161220000 apple Linux apple-VirtualBox 5.13.0-28-generic #31~20.04.1-Ubuntu SMP Wed Jan 19 14:08:10 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:59:05 up  1:23,  1 user,  load average: 0.17, 0.30, 0.20 672ce22bc6e93bec1ccf27feb07451301fa5f744

--- nexus-am/apps/typing/game.c
+++ nexus-am/apps/typing/game.c
@@ -30,7 +30,6 @@ int main (){
     next_frame += 1000 / HZ;
 
     while (keyboard_event());
-    printf("aaaaaaaa\n");
     while (update_keypress());
      
     if (frames % (HZ / CHARACTER_PER_SECOND) == 0) {
