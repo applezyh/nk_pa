4fd15df26ff8e4e26bf1f7a9d508c64b8778eef8
tracer-ics2017 <tracer@njuics.org>
2022-02-20 11:49:28 +0800

    >  compile 161220000 apple Linux apple-VirtualBox 5.13.0-28-generic #31~20.04.1-Ubuntu SMP Wed Jan 19 14:08:10 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:49:28 up 1 day, 19:30,  1 user,  load average: 0.09, 0.11, 0.16 c96d2e4a4709a17399e590e0b79084923eb18d41

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -1,5 +1,5 @@
 #include "nemu.h"
-
+#include "device/mmio.h"
 #define PMEM_SIZE (128 * 1024 * 1024)
 
 #define pmem_rw(addr, type) *(type *)({\
@@ -12,11 +12,14 @@ uint8_t pmem[PMEM_SIZE];
 /* Memory accessing interfaces */
 
 uint32_t paddr_read(paddr_t addr, int len) {
-  return pmem_rw(addr, uint32_t) & (~0u >> ((4 - len) << 3));
+  int NO = is_mmio(addr);
+  return NO==-1?pmem_rw(addr, uint32_t) & (~0u >> ((4 - len) << 3)):mmio_read(addr,len,NO);
 }
 
 void paddr_write(paddr_t addr, int len, uint32_t data) {
-  memcpy(guest_to_host(addr), &data, len);
+  int NO = is_mmio(addr);
+  if(NO==-1) memcpy(guest_to_host(addr), &data, len);
+  else mmio_write(addr,len,data,NO);
 }
 
 uint32_t vaddr_read(vaddr_t addr, int len) {
