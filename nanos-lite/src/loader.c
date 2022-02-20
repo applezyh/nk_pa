ed1071e8978b59b7735d002cd7502508e8a89adb
tracer-ics2017 <tracer@njuics.org>
2022-02-20 23:16:47 +0800

    >  run 161220000 apple Linux apple-VirtualBox 5.13.0-28-generic #31~20.04.1-Ubuntu SMP Wed Jan 19 14:08:10 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:16:47 up 47 min,  1 user,  load average: 0.00, 0.01, 0.02 9aaefc4a257336e0c4414626602b717c5cda3

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -4,6 +4,6 @@
 
 uintptr_t loader(_Protect *as, const char *filename) {
   // TODO();
-  ramdisk_read(DEFAULT_ENTRY,0,as->area.start-as->area.end);
+  ramdisk_read(DEFAULT_ENTRY,0,as->area.end-as->area.start);
   return (uintptr_t)DEFAULT_ENTRY;
 }
