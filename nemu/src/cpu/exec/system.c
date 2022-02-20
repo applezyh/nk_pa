f72cf7b089cc795adaac8cd0658809d86600af9d
tracer-ics2017 <tracer@njuics.org>
2022-02-20 09:37:44 +0800

    >  compile 161220000 apple Linux apple-VirtualBox 5.13.0-28-generic #31~20.04.1-Ubuntu SMP Wed Jan 19 14:08:10 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  09:37:44 up 1 day, 17:18,  1 user,  load average: 0.06, 0.11, 0.09 3c7f06d5f9ac7824ed40c8eeaac80506cacfb6

--- nemu/src/cpu/exec/system.c
+++ nemu/src/cpu/exec/system.c
@@ -56,6 +56,7 @@ make_EHelper(in) {
 
 make_EHelper(out) {
   // TODO();
+  printf("%d %d\n",id_src->width,id_dest->width);
   pio_write(id_dest->val,id_src->val,id_src->width);
   print_asm_template2(out);
 
