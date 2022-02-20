df78db6e6dedebdc7432275e0570f94b9e82b9dd
tracer-ics2017 <tracer@njuics.org>
2022-02-20 12:07:48 +0800

    >  compile 161220000 apple Linux apple-VirtualBox 5.13.0-28-generic #31~20.04.1-Ubuntu SMP Wed Jan 19 14:08:10 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  12:07:48 up 1 day, 19:48,  1 user,  load average: 0.23, 0.21, 0.17 2812221fba2e3c2d3c132535601a201961c032da

--- nemu/src/device/io/mmio.c
+++ nemu/src/device/io/mmio.c
@@ -35,7 +35,6 @@ void* add_mmio_map(paddr_t addr, int len, mmio_callback_t callback) {
 /* bus interface */
 int is_mmio(paddr_t addr) {
   int i;
-  assert(addr>0xfffffffff);
   for (i = 0; i < nr_map; i ++) {
     if (addr >= maps[i].low && addr <= maps[i].high) {
       return i;
