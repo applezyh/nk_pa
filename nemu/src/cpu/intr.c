82dbdf45be6148daa55ef8882978e514ea653017
tracer-ics2017 <tracer@njuics.org>
2022-02-21 15:32:42 +0800

    >  compile 161220000 apple Linux apple-VirtualBox 5.13.0-28-generic #31~20.04.1-Ubuntu SMP Wed Jan 19 14:08:10 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:32:42 up  2:11,  1 user,  load average: 0.08, 0.09, 0.24 f5d7663cd3896fc5e78625ca920d1a76e8154c3e

--- nemu/src/cpu/intr.c
+++ nemu/src/cpu/intr.c
@@ -6,7 +6,11 @@ void raise_intr(uint8_t NO, vaddr_t ret_addr) {
    * That is, use ``NO'' to index the IDT.
    */
 
-  TODO();
+  //TODO();
+  rtl_push(cpu.eflag);
+  rtl_push(ret_addr);
+  rtl_push(cpu.cs);
+  cpu.eip=((vaddr_t*)(cpu.IDTentry))[NO];
 }
 
 void dev_raise_intr() {
