fb7b9fc70d3af4489702145fbe587a275708bef2
tracer-ics2017 <tracer@njuics.org>
2022-02-21 23:01:51 +0800

    >  run 161220000 apple Linux apple-VirtualBox 5.13.0-28-generic #31~20.04.1-Ubuntu SMP Wed Jan 19 14:08:10 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:01:51 up  2:47,  1 user,  load average: 0.23, 0.17, 0.08 ee11fa0bc7d02356f3c6aebb814396465fc03f84

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -39,11 +39,11 @@ int sys_write(int fd,uint8_t* start,uint32_t len){
 _RegSet* do_syscall(_RegSet *r) {
   uintptr_t a[4];
   a[0] = SYSCALL_ARG1(r);
-  printf("call %d\n",a[0]);
+  printf("call %d %d\n",a[0],SYSCALL_ARG2(r));
   switch (a[0]) {
     case SYS_none: SYSCALL_ARG1(r)=sys_nano(); break;
-    case SYS_exit: sys_exit(SYSCALL_ARG2(r)); break;
     case SYS_write: sys_write(SYSCALL_ARG2(r),(uint8_t*)SYSCALL_ARG3(r),SYSCALL_ARG4(r)); break;
+    case SYS_exit: sys_exit(SYSCALL_ARG2(r)); break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 
