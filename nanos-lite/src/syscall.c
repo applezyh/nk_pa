19f90aa1959134bedbda96709e44fa53be4998f4
tracer-ics2017 <tracer@njuics.org>
2022-02-21 21:29:48 +0800

    >  run 161220000 apple Linux apple-VirtualBox 5.13.0-28-generic #31~20.04.1-Ubuntu SMP Wed Jan 19 14:08:10 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:29:48 up  1:37,  1 user,  load average: 0.49, 0.22, 0.08 29d98b2d77a90e315ced652269970ee2c660caf9

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -10,7 +10,7 @@ _RegSet* do_syscall(_RegSet *r) {
   a[0] = SYSCALL_ARG1(r);
 
   switch (a[0]) {
-    case SYS_none: SYSCALL_ARG1(r)=sys_nano();
+    case SYS_none: SYSCALL_ARG1(r)=sys_nano(); break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 
