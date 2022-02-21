f0cf0734cb918b76d1a11b186401eff68b4d68ce
tracer-ics2017 <tracer@njuics.org>
2022-02-21 16:32:17 +0800

    >  compile 161220000 apple Linux apple-VirtualBox 5.13.0-28-generic #31~20.04.1-Ubuntu SMP Wed Jan 19 14:08:10 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:32:17 up  3:10,  1 user,  load average: 0.08, 0.02, 0.01 ec3fd848d749a3e79938b6dd1c7223cdbfa876f9

--- nemu/src/cpu/intr.c
+++ nemu/src/cpu/intr.c
@@ -10,10 +10,9 @@ void raise_intr(uint8_t NO, vaddr_t ret_addr) {
   rtl_push(&cpu.eflag);
   rtl_push(&ret_addr);
   rtl_push(&cpu.cs);
-  printf("%lx\n",cpu.IDTentry+NO*sizeof(GateDesc));
   uint32_t gate[2];
-  gate[1] = vaddr_read(cpu.IDTentry+NO*sizeof(GateDesc),4);
-  gate[2] = vaddr_read(cpu.IDTentry+NO*sizeof(GateDesc)+4,4);
+  gate[0] = vaddr_read(cpu.IDTentry+NO*sizeof(GateDesc),4);
+  gate[1] = vaddr_read(cpu.IDTentry+NO*sizeof(GateDesc)+4,4);
   GateDesc g = (*((GateDesc*)gate));
   cpu.eip= g.offset_15_0+(g.offset_31_16<<16);
   printf("%x\n",cpu.eip);
