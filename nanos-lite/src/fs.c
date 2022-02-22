6f1b6dd5928340ba97d0761862b8d6e80cded0dc
tracer-ics2017 <tracer@njuics.org>
2022-02-22 14:06:59 +0800

    >  run 161220000 apple Linux apple-VirtualBox 5.13.0-28-generic #31~20.04.1-Ubuntu SMP Wed Jan 19 14:08:10 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:06:59 up  1:26,  1 user,  load average: 0.03, 0.05, 0.02 d949da67a61796b670eb06da53464e23cb6f637

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -1,25 +1,69 @@
 #include "fs.h"
-
+#include "ramdisk.h"
 typedef struct {
   char *name;
   size_t size;
   off_t disk_offset;
+  off_t open_offset;
 } Finfo;
 
 enum {FD_STDIN, FD_STDOUT, FD_STDERR, FD_FB, FD_EVENTS, FD_DISPINFO, FD_NORMAL};
 
 /* This is the information about all files in disk. */
 static Finfo file_table[] __attribute__((used)) = {
-  {"stdin (note that this is not the actual stdin)", 0, 0},
-  {"stdout (note that this is not the actual stdout)", 0, 0},
-  {"stderr (note that this is not the actual stderr)", 0, 0},
-  [FD_FB] = {"/dev/fb", 0, 0},
-  [FD_EVENTS] = {"/dev/events", 0, 0},
-  [FD_DISPINFO] = {"/proc/dispinfo", 128, 0},
+  {"stdin (note that this is not the actual stdin)", 0, 0, 0},
+  {"stdout (note that this is not the actual stdout)", 0, 0, 0},
+  {"stderr (note that this is not the actual stderr)", 0, 0, 0},
+  [FD_FB] = {"/dev/fb", 0, 0, 0},
+  [FD_EVENTS] = {"/dev/events", 0, 0, 0},
+  [FD_DISPINFO] = {"/proc/dispinfo", 128, 0, 0},
 #include "files.h"
 };
-
+size_t fs_filesz(int fd){
+  return file_table[fd].size;
+}
 #define NR_FILES (sizeof(file_table) / sizeof(file_table[0]))
+
+int fs_open(const char *pathname, int flags, int mode){
+  for(size_t i=0;i<NR_FILES;i++){
+    if(strcmp(pathname,file_table[i].name)){
+      return i;
+    }
+  }
+  return -1;
+}
+ssize_t fs_read(int fd, void *buf, size_t len){
+  off_t disk_off = file_table[fd].disk_offset;
+  off_t open_off = file_table[fd].open_offset;
+  ramdisk_read(buf,disk_off+open_off,len);
+  file_table[fd].open_offset+=len;
+  return len;
+}
+ssize_t fs_write(int fd, const void *buf, size_t len){
+  
+  off_t disk_off = file_table[fd].disk_offset;
+  off_t open_off = file_table[fd].open_offset;
+  assert(open_off+len<file_table[fd].size);
+  ramdisk_write(buf,disk_off+open_off,len);
+  file_table[fd].open_offset+=len;
+  return len;
+}
+off_t fs_lseek(int fd, off_t offset, int whence){
+  switch (whence)
+  {
+  case SEEK_SET: file_table[fd].open_offset=offset;break;
+  case SEEK_CUR: file_table[fd].open_offset+=offset;break;
+  case SEEK_END: file_table[fd].open_offset=(file_table[fd].size+file_table[fd].disk_offset+offset);break;
+    /* code */
+  default:
+    assert(0);
+  }
+  return file_table[fd].open_offset+file_table[fd].disk_offset;
+}
+int fs_close(int fd){
+  file_table[fd].open_offset=0;
+  return 0;
+}
 
 void init_fs() {
   // TODO: initialize the size of /dev/fb
--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -1,10 +1,12 @@
 #include "common.h"
 #include "ramdisk.h"
+#include "fs.h"
 #define DEFAULT_ENTRY ((void *)0xc000000)
 extern uint8_t ramdisk_start;
 extern uint8_t ramdisk_end;
 uintptr_t loader(_Protect *as, const char *filename) {
   // TODO();
-  ramdisk_read(DEFAULT_ENTRY,0,RAMDISK_SIZE);
+  int fd=fs_open(filename,0,0);
+  fs_read(fd,DEFAULT_ENTRY,RAMDISK_SIZE);
   return (uintptr_t)DEFAULT_ENTRY;
 }
