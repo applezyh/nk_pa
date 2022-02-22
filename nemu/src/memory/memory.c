a3e629c94eb1d4b55f7edf7a66edc0196d6a4db1
tracer-ics2017 <tracer@njuics.org>
2022-02-22 12:42:14 +0800

    >  compile 161220000 apple Linux apple-VirtualBox 5.13.0-28-generic #31~20.04.1-Ubuntu SMP Wed Jan 19 14:08:10 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  12:42:14 up 1 min,  1 user,  load average: 0.75, 0.26, 0.09 2d07cd967ffd4533363e2d8231ba07a756ec043b

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -18,7 +18,7 @@ uint32_t paddr_read(paddr_t addr, int len) {
 
 void paddr_write(paddr_t addr, int len, uint32_t data) {
   if(addr>=0xc000000){
-    printf("eip : write to %x\n",cpu.eip,addr);
+    printf("eip : %x write to %x\n",cpu.eip,addr);
   }
   int NO = is_mmio(addr);
   if(NO==-1) memcpy(guest_to_host(addr), &data, len);
