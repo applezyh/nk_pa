06087362248a2fb2ed40a434a21d1346a2a0b88b
tracer-ics2017 <tracer@njuics.org>
2022-02-22 14:10:48 +0800

    >  run 161220000 apple Linux apple-VirtualBox 5.13.0-28-generic #31~20.04.1-Ubuntu SMP Wed Jan 19 14:08:10 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:10:48 up  1:29,  1 user,  load average: 0.13, 0.08, 0.02 78c599795c792ee6efaba095aa4b5a3a37b53bd1

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -6,6 +6,7 @@ extern uint8_t ramdisk_start;
 extern uint8_t ramdisk_end;
 uintptr_t loader(_Protect *as, const char *filename) {
   // TODO();
+  printf("%s\n",filename);
   int fd=fs_open(filename,0,0);
   fs_read(fd,DEFAULT_ENTRY,RAMDISK_SIZE);
   return (uintptr_t)DEFAULT_ENTRY;
