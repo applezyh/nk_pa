453c30e7a94b5183279066fed7dc00028c138fde
tracer-ics2017 <tracer@njuics.org>
2022-02-22 11:39:40 +0800

    >  run 161220000 apple Linux apple-VirtualBox 5.13.0-28-generic #31~20.04.1-Ubuntu SMP Wed Jan 19 14:08:10 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:39:40 up  2:16,  1 user,  load average: 0.00, 0.03, 0.08 46c202302d70630fdc83d4e3156698684193903a

--- nexus-am/am/arch/x86-nemu/src/asye.c
+++ nexus-am/am/arch/x86-nemu/src/asye.c
@@ -2,7 +2,7 @@
 #include <x86.h>
 #include <klib.h>
 
-static _RegSet* (*H)(_Event, _RegSet*) = NULL;
+_RegSet* (*H)(_Event, _RegSet*) = NULL;
 
 void vecsys();
 void vecnull();
@@ -25,7 +25,7 @@ _RegSet* irq_handle(_RegSet *tf) {
   return next;
 }
 
-GateDesc idt[NR_IRQ];
+GateDesc idt[NR_IRQ]={};
 
 void _asye_init(_RegSet*(*h)(_Event, _RegSet*)) {
   // initialize IDT
