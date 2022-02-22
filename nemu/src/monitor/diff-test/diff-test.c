d8d52bde85881f60b3ff20137761b3cb1e924751
tracer-ics2017 <tracer@njuics.org>
2022-02-22 17:07:27 +0800

    >  compile 161220000 apple Linux apple-VirtualBox 5.13.0-28-generic #31~20.04.1-Ubuntu SMP Wed Jan 19 14:08:10 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:07:27 up  2:08,  1 user,  load average: 0.00, 0.07, 0.09 ea1b57d8d1fda2615eae38029ee31e7696d4298f

--- nemu/src/monitor/diff-test/diff-test.c
+++ nemu/src/monitor/diff-test/diff-test.c
@@ -162,10 +162,10 @@ void difftest_step(uint32_t eip) {
   }
   // printf("%x %x\n",r.array[8],cpu.eip);
   // printf("%x %x\n",r.array[9],cpu.eflag);
-  flag=(r.eip!=cpu.eip);
+  flag=(r.eip!=eip);
   diff|=flag;
   if (flag) {
-      printf("diff eip true:%x but %x\n",r.eip,cpu.eip);
+      printf("diff eip true:%x but %x\n",r.eip,eip);
     }
   if (diff) {
     nemu_state = NEMU_STOP;
