0b4da786f4bcc34bf272eea01f0699c66c8a2609
tracer-ics2017 <tracer@njuics.org>
2022-02-22 18:47:24 +0800

    >  run 161220000 apple Linux apple-VirtualBox 5.13.0-28-generic #31~20.04.1-Ubuntu SMP Wed Jan 19 14:08:10 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:47:24 up  3:48,  1 user,  load average: 0.15, 0.12, 0.09 2f6c473d3ebb8a33521c4172719386f7fb8027

--- navy-apps/libs/libos/src/nanos.c
+++ navy-apps/libs/libos/src/nanos.c
@@ -27,7 +27,7 @@ int _open(const char *path, int flags, mode_t mode) {
 
 int _write(int fd, void *buf, size_t count){
   _syscall_(SYS_write, fd, (uintptr_t)buf, count);
-  return 0;
+  return 1;
 }
 
 extern void* _end;
