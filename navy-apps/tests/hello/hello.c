6192066a503c1e777514b2ddde47ba845686e7fb
tracer-ics2017 <tracer@njuics.org>
2022-02-22 18:49:17 +0800

    >  run 161220000 apple Linux apple-VirtualBox 5.13.0-28-generic #31~20.04.1-Ubuntu SMP Wed Jan 19 14:08:10 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:49:17 up  3:50,  1 user,  load average: 0.27, 0.18, 0.11 eec7daa24efe0a20cc441a3a605f1a8d325c00dc

--- navy-apps/tests/hello/hello.c
+++ navy-apps/tests/hello/hello.c
@@ -7,7 +7,7 @@ int main() {
   volatile int j = 0;
   while (j<10) {
     j ++;
-    printf("Hello World for the %dth time\n", i ++);
+    printf("Hello World for the %dth time\n", j ++);
   }
   return 0;
 }
