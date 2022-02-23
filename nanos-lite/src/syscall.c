bcdc95d471804cf1e7b68f08a877c97024bc327f
tracer-ics2017 <tracer@njuics.org>
2022-02-23 20:58:33 +0800

    >  compile 161220000 apple Linux apple-VirtualBox 5.13.0-28-generic #31~20.04.1-Ubuntu SMP Wed Jan 19 14:08:10 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:58:33 up  9:05,  1 user,  load average: 0.03, 0.08, 0.09 fb745d674ed75397182449f219f350f71a19dd0f

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -42,10 +42,10 @@ int sys_write(int fd,const char* start,uint32_t len){
 }
 
 extern void* _end;
-extern void* _etext;
+extern void* _edata;
 int sys_brk(uintptr_t inc){
   printf("_end = %p\n",_end);
-  printf("_etext = %p\n",_etext);
+  printf("_edata = %p\n",_edata);
   _end=(void*)inc;
   return 0;
 }
