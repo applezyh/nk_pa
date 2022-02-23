5cbe2dc9d6402eb6310d1b700d24c89ff0f2325f
tracer-ics2017 <tracer@njuics.org>
2022-02-23 11:40:04 +0800

    >  run 161220000 apple Linux apple-VirtualBox 5.13.0-28-generic #31~20.04.1-Ubuntu SMP Wed Jan 19 14:08:10 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:40:04 up  8:00,  1 user,  load average: 0.38, 0.17, 0.19 727a4e96384eee2054603790de2e34dbffc20c9a

--- navy-apps/libs/libc/src/stdio/findfp.c
+++ navy-apps/libs/libc/src/stdio/findfp.c
@@ -81,7 +81,6 @@ __sfp (d)
   for (g = &d->__sglue;; g = g->_next)
     {
       for (fp = g->_iobs, n = g->_niobs; --n >= 0; fp++){
-        printf("%d\n",fp->_flags);
 	      if (fp->_flags == 0)
 	        goto found;
       }
@@ -145,7 +144,7 @@ __sinit (s)
   /* make sure we clean up on exit */
   s->__cleanup = _cleanup_r;	/* conservative */
   s->__sdidinit = 1;
-
+  printf("%d %d %d\n",__SRD,__SWR | __SLBF,__SWR | __SNBF);
   std (s->__sf + 0, __SRD, 0, s);
   std (s->__sf + 1, __SWR | __SLBF, 1, s);
   std (s->__sf + 2, __SWR | __SNBF, 2, s);
