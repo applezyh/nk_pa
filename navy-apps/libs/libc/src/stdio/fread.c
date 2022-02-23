4d139c9137424fd70c62fa5f3a99b15a17628154
tracer-ics2017 <tracer@njuics.org>
2022-02-23 23:59:20 +0800

    >  run 161220000 apple Linux apple-VirtualBox 5.13.0-28-generic #31~20.04.1-Ubuntu SMP Wed Jan 19 14:08:10 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:59:20 up 12:06,  1 user,  load average: 0.13, 0.15, 0.11 e3fe9171867f7e0e31ba6b4ae00ba28a98ec7b7

--- navy-apps/libs/libc/src/stdio/fread.c
+++ navy-apps/libs/libc/src/stdio/fread.c
@@ -84,16 +84,15 @@ _DEFUN (fread, (buf, size, count, fp),
       /* fp->_r = 0 ... done in __srefill */
       p += r;
       resid -= r;
-     
+     printf("begin fill\n");
       if (__srefill (fp))
 	{
 	  /* no more input: return partial result */
 	  return (total - resid) / size;
 	}
+  printf("end fill\n");
   } 
-  printf("copy\n");
   (void) memcpy ((void *) p, (void *) fp->_p, resid);
-  printf("end copy\n");
   fp->_r -= resid;
   fp->_p += resid;
   return count;
