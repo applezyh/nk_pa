7ac8b9f8e2da01d270aeb092df563264e6faa75c
tracer-ics2017 <tracer@njuics.org>
2022-02-23 21:43:00 +0800

    >  run 161220000 apple Linux apple-VirtualBox 5.13.0-28-generic #31~20.04.1-Ubuntu SMP Wed Jan 19 14:08:10 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:43:00 up  9:50,  1 user,  load average: 0.26, 0.17, 0.11 dcf7c68b22be1795361c5ff23cb23afc3362510

--- navy-apps/tests/text/text.c
+++ navy-apps/tests/text/text.c
@@ -7,7 +7,7 @@ int main() {
   fseek(fp, 0, SEEK_END);
   long size = ftell(fp);
   int a=size;
-  printf("%ld\n",a);
+  printf("%u\n",a);
   assert(size == 5000);
 
   fseek(fp, 500 * 5, SEEK_SET);
