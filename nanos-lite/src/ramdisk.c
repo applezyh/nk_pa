17aac02742029b5da0313d9f8c87c48e981147d0
tracer-ics2017 <tracer@njuics.org>
2022-02-23 22:59:19 +0800

    >  run 161220000 apple Linux apple-VirtualBox 5.13.0-28-generic #31~20.04.1-Ubuntu SMP Wed Jan 19 14:08:10 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:59:19 up 11:06,  1 user,  load average: 0.00, 0.13, 0.17 56ccc044def32454d552cef8311f9a81e60fd406

--- nanos-lite/src/ramdisk.c
+++ nanos-lite/src/ramdisk.c
@@ -10,7 +10,6 @@
 void ramdisk_read(void *buf, off_t offset, size_t len) {
   assert(offset + len <= RAMDISK_SIZE);
   memcpy(buf, &ramdisk_start + offset, len);
-  printf("copy %d\n",len);
 }
 
 /* write `len' bytes starting from `buf' into the `offset' of ramdisk */
