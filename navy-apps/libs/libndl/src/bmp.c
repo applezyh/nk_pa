6210c4f6eeb96c66508579caea9848c991817495
tracer-ics2017 <tracer@njuics.org>
2022-02-23 23:47:12 +0800

    >  run 161220000 apple Linux apple-VirtualBox 5.13.0-28-generic #31~20.04.1-Ubuntu SMP Wed Jan 19 14:08:10 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:47:12 up 11:54,  1 user,  load average: 0.37, 0.20, 0.13 68926631a22ba74dab6a11c2746041bb75e3eeb4

--- navy-apps/libs/libndl/src/bmp.c
+++ navy-apps/libs/libndl/src/bmp.c
@@ -27,10 +27,11 @@ int NDL_LoadBitmap(NDL_Bitmap *bmp, const char *filename) {
   w = h = 0;
   printf("open\n");
   if (!(fp = fopen(filename, "r"))) return -1;
-  
+  printf("begin read\n");
   struct BitmapHeader hdr;
   assert(sizeof(hdr) == 54);
   assert(1 == fread(&hdr, sizeof(struct BitmapHeader), 1, fp));
+  printf("end read\n");
   if (hdr.bitcount != 24) return -1;
   if (hdr.compression != 0) return -1;
   pixels = (uint32_t*)malloc(hdr.width * hdr.height * sizeof(uint32_t));
