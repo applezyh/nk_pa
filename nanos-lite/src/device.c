6020f3f6ce05301ebe12bde370ca59e0944dc7ce
tracer-ics2017 <tracer@njuics.org>
2022-02-24 00:31:11 +0800

    >  run 161220000 apple Linux apple-VirtualBox 5.13.0-28-generic #31~20.04.1-Ubuntu SMP Wed Jan 19 14:08:10 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  00:31:11 up 12:38,  1 user,  load average: 0.37, 0.32, 0.27 bdfec3064f3713f19196223dde7fab4154508568

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -27,7 +27,7 @@ void fb_write(const void *buf, off_t offset, size_t len) {
 }
 
 void init_device() {
-  memcpy(dispinfo,"screen width:400 height 300",28);
+  memcpy(dispinfo,"WIDTH:400 HEIGHT:300",28);
   _ioe_init();
 
   // TODO: print the string to array `dispinfo` with the format
