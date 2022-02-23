6cbd5ba463ff34fbe1f15fe96e542a99f5e83c65
tracer-ics2017 <tracer@njuics.org>
2022-02-24 02:48:27 +0800

    >  run 161220000 apple Linux apple-VirtualBox 5.13.0-28-generic #31~20.04.1-Ubuntu SMP Wed Jan 19 14:08:10 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  02:48:27 up  1:28,  1 user,  load average: 0.59, 0.37, 0.38 bbcd705dd6a7ed1e50c3e047701c04424346bd04

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -22,12 +22,7 @@ void dispinfo_read(void *buf, off_t offset, size_t len) {
 void fb_write(const void *buf, off_t offset, size_t len) {
     offset/=4;
     len/=4;
-    uint32_t x = offset / (_screen.width);
-    uint32_t y = offset % (_screen.width);
-    uint32_t h = (offset + len) / (_screen.width) - x;
-    uint32_t w = (offset + len) % (_screen.width) - y;
-    //printf("offet: %d %d %d %d %d\n",offset,x,y,x+h,y+w);
-    _draw_rect(buf,offset,len,w,h,1);
+    _draw_rect(buf,offset,len,0,0,1);
 }
 
 void init_device() {
