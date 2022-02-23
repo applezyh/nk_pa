463f2ed52539f91955bec9e04c935ee96104ea14
tracer-ics2017 <tracer@njuics.org>
2022-02-23 20:24:21 +0800

    >  run 161220000 apple Linux apple-VirtualBox 5.13.0-28-generic #31~20.04.1-Ubuntu SMP Wed Jan 19 14:08:10 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:24:21 up  8:31,  1 user,  load average: 0.41, 0.12, 0.09 a335281ee06429d8ac27e02d62dbf2e89301f8a4

--- navy-apps/libs/libos/src/nanos.c
+++ navy-apps/libs/libos/src/nanos.c
@@ -36,7 +36,9 @@ extern void* _end;
 void* _sbrk(intptr_t increment){
   void* old=_end;
   if(0 == _syscall_(SYS_brk, (uintptr_t)(_end+increment), 0, 0)){
-    printf("%p\n",old);
+    char a[100];
+    int s=sprintf(a,"%p\n",old);
+    _write(1,a,s);
     return old;
   }
   return (void*)-1;
