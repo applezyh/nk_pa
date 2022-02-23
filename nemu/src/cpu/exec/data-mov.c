11961d195ed9bddc19eb274c7ad1b1f3728d57be
tracer-ics2017 <tracer@njuics.org>
2022-02-23 23:14:25 +0800

    >  compile 161220000 apple Linux apple-VirtualBox 5.13.0-28-generic #31~20.04.1-Ubuntu SMP Wed Jan 19 14:08:10 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:14:25 up 11:21,  1 user,  load average: 0.05, 0.05, 0.10 c547d1c4569de5a7659ab4e5ce10b2e9706c31d

--- nemu/src/cpu/exec/data-mov.c
+++ nemu/src/cpu/exec/data-mov.c
@@ -225,6 +225,7 @@ make_EHelper(movzx) {
 }
 
 make_EHelper(movsb) {
+  printf("%x to %x\n",cpu.ds+cpu.esi,cpu.es+cpu.edi);
   rtl_get_ZF(&t0);
   uint8_t data=vaddr_read(cpu.ds+cpu.esi,1);
   vaddr_write(cpu.es+cpu.edi,1,data);
