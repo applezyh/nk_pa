d7ae9e2d540571cc86bf64500bceb8ac1997f745
tracer-ics2017 <tracer@njuics.org>
2022-05-15 17:12:18 +0800

    >  run 161220000 root Linux apple-VirtualBox 5.13.0-40-generic #45~20.04.1-Ubuntu SMP Mon Apr 4 09:38:31 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:12:18 up  2:48,  1 user,  load average: 0.01, 0.01, 0.02 775510c8bd962cffd1d05d9bf4e2f83d31a01c92

--- nexus-am/am/arch/x86-nemu/src/ioe.c
+++ nexus-am/am/arch/x86-nemu/src/ioe.c
@@ -28,16 +28,10 @@ extern void* memcpy(void *, const void *, int);
 
 void _draw_rect(const uint32_t *pixels, int x, int y, int w, int h,int s) {
   if(s){
-    for(int i=x;i<x+y;i++){
-      fb[i]=pixels[i-x];
+    for(int i=x;i<x+y;i++) {
+      fb[i] = pixels[i-x];
     }
     return;
-  }
-  uint32_t seq=0;
-  for(int i=y;i<y+h;i++){
-    for(int j=x;j<x+w;j++){
-      fb[i*_screen.width+j]=pixels[seq++];
-    }
   }
 }
 
