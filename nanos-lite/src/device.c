af02ee23c269f15b9ce80af6df4dd414ca3109ff
tracer-ics2017 <tracer@njuics.org>
2022-05-15 16:30:17 +0800

    >  run 161220000 root Linux apple-VirtualBox 5.13.0-40-generic #45~20.04.1-Ubuntu SMP Mon Apr 4 09:38:31 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:30:17 up  2:06,  1 user,  load average: 0.18, 0.18, 0.16 dc88cd3c26b60f9ef3d48163251e8dc828063c2a

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -37,8 +37,8 @@ void dispinfo_read(void *buf, off_t offset, size_t len) {
 
 
 void fb_write(const void *buf, off_t offset, size_t len) {
-    //offset;
-    len/=4;
+    offset/=4;
+    //len;
     _draw_rect(buf,offset,len,0,0,1);
 }
 
