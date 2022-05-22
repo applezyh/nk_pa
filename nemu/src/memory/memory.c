2dbf8aaee8bfc18eb083f1e8bccfb0d612492a4e
tracer-ics2017 <tracer@njuics.org>
2022-05-22 18:29:32 +0800

    >  compile 161220000 root Linux apple-VirtualBox 5.13.0-40-generic #45~20.04.1-Ubuntu SMP Mon Apr 4 09:38:31 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:29:32 up 17:02,  1 user,  load average: 0.15, 0.09, 0.08 7581dbf377445959fddd93465072568dc2ce2359

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -89,6 +89,7 @@ uint32_t vaddr_read(vaddr_t addr, int len) {
 	}
 	else {
 		paddr_t paddr = page_translate(addr, false);
+		Log("%x", paddr);
 		return paddr_read(paddr, len);
 	}
   //return paddr_read(addr, len);
