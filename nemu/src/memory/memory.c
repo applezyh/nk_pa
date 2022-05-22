48ed593e856bf9a8c88d3bb9e180e23269beb3fd
tracer-ics2017 <tracer@njuics.org>
2022-05-22 23:02:03 +0800

    >  compile 161220000 root Linux apple-VirtualBox 5.13.0-40-generic #45~20.04.1-Ubuntu SMP Mon Apr 4 09:38:31 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:02:03 up 21:35,  1 user,  load average: 0.27, 0.08, 0.04 ce77c344a96f357e3232c084e1a6cdd180b86e0d

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -38,12 +38,12 @@ uint32_t page_translate(vaddr_t addr, bool iswrite) {
   if (cr0.protect_enable && cr0.paging) {
     pgdir = (PDE *)(intptr_t)(cr3.page_directory_base << 12);
     pde.val = paddr_read((intptr_t)&pgdir[(addr >> 22) & 0x3ff], 4);
-    Assert(pde.present, "PDE: 0x%x", pde);
+    Assert(pde.present, "addr: 0x%x PDE: 0x%x", addr, pde);
     pde.accessed = 1;
 
     pgtab = (PTE *)(intptr_t)(pde.page_frame << 12);
     pte.val = paddr_read((intptr_t)&pgtab[(addr >> 12) & 0x3ff], 4);
-    Assert(pte.present, "PTE: 0x%x", pte);
+    Assert(pte.present, "addr: 0x%x PTE: 0x%x", addr, pte);
     pte.accessed = 1;
     pte.dirty = iswrite ? 1 : pte.dirty;
 
