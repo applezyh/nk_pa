590f1698ca08e991547976bf847791bae292b4f8
tracer-ics2017 <tracer@njuics.org>
2022-05-22 19:35:44 +0800

    >  compile 161220000 root Linux apple-VirtualBox 5.13.0-40-generic #45~20.04.1-Ubuntu SMP Mon Apr 4 09:38:31 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:35:44 up 18:09,  1 user,  load average: 0.00, 0.08, 0.13 be94cf61534326c178416aabe31eec3c9aa75cb

--- nemu/src/cpu/decode/decode.c
+++ nemu/src/cpu/decode/decode.c
@@ -345,7 +345,7 @@ make_DHelper(mov_load_cr){
 }
 
 make_DHelper(mov_store_cr){
-  decode_op_rm(eip, id_dest, false, id_src, false);
+  decode_op_rm(eip, id_src, false, id_dest, false);
 #ifdef DEBUG
   sprintf(id_dest->str, 5, "%%cr%d", id_dest->reg);
 #endif
