08dc15e0fa42195b0595633315e0edc8ac83daf7
tracer-ics2017 <tracer@njuics.org>
2022-05-22 22:23:07 +0800

    >  run 161220000 root Linux apple-VirtualBox 5.13.0-40-generic #45~20.04.1-Ubuntu SMP Mon Apr 4 09:38:31 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:23:07 up 20:56,  1 user,  load average: 0.00, 0.02, 0.00 eb7f50df5c4b046fb54e8c0849053a076b28b755

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -1,10 +1,12 @@
 #include "common.h"
+extern _RegSet* schedule(_RegSet *prev);
+extern _RegSet* do_syscall(_RegSet *prev);
 #define _EVENT_SYSCALL 8
 extern _RegSet* do_syscall(_RegSet *r);
 static _RegSet* do_event(_Event e, _RegSet* r) {
   switch (e.event) {
     case _EVENT_SYSCALL: do_syscall(r); break;
-    case _EVENT_TRAP: printf("event:self-trapped\n"); break; 
+    case _EVENT_TRAP: printf("event:self-trapped\n"); schedule(r); break; 
     default: panic("Unhandled event ID = %d", e.event);
   }
 
