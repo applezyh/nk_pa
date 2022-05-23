63db7dea5f94393e7e3da8af6675c4e80a3dcd84
tracer-ics2017 <tracer@njuics.org>
2022-05-23 11:15:03 +0800

    >  run 161220000 root Linux apple-VirtualBox 5.13.0-40-generic #45~20.04.1-Ubuntu SMP Mon Apr 4 09:38:31 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:15:03 up 1 day, 44 min,  1 user,  load average: 1.10, 0.56, 0.30 2a7dbbf0b96e4e56755753da23f2adb58c5ba0e6

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -41,7 +41,7 @@ _RegSet* schedule(_RegSet *prev) {
    current=&pcb[1];
    num=0;
   }
-  Log("ptr=0x%x", (uint32_t)current->as.ptr);
+  //Log("ptr=0x%x", (uint32_t)current->as.ptr);
   _switch(&current->as);
   //Log("schedule return eip %x", current->tf->eip);
   return current->tf;
